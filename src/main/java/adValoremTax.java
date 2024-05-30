88c9a0123a1cdf2e5896af6f511c0d7d8284c91f
Alperen Yilmaz <theurgeboat@gmail.com>
2024-05-31 01:28:17 +0300

    adValoremTax | fix: try-catch block for non-float inputs

--- src/main/java/adValoremTax.java
+++ src/main/java/adValoremTax.java
@@ -1,14 +1,32 @@
 import java.util.Scanner;
-public class adValoremTax   // can be improved with try-catch number format exception
+public class adValoremTax
 {
     public static void main(String[] args)
     {
         Scanner tara = new Scanner(System.in);
-        System.out.printf("Enter amount (fractions allowed):");
-        double tutar = tara.nextDouble();
+        float amount=0;
+        while (true | false == true)    // because why not
+        {
+            System.out.printf("(Blank enter to exit) Enter amount (fractions allowed):");
+            String entry = tara.nextLine();
+            if (entry.isEmpty())
+            {
+                System.out.println("Exiting...");
+                break;
+            }
 
-        double KDVoran = (tutar<1000) ? 0.18:0.08;  // cool logic
+            try
+            {
+                amount = Float.parseFloat(entry.trim());
 
-        System.out.printf("\nTax rate: %.2f\nPrice with taxes added = %.2f", KDVoran, (tutar*(1+KDVoran)));
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input! Please enter a valid number.");
+            }
+
+        }
+
+        float KDVoran = (float) ((amount < 1000) ? 0.18 : 0.08);  // cool logic, if tutar<1000 is TRUE, KDVoran=0.18
+        System.out.printf("\nTax rate: %.2f\nPrice with taxes added = %.2f", KDVoran, (amount * (1 + KDVoran)));
     }
 }
