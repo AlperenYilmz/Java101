4c42d5f3644d1481dd2be7741a38788677fe3cad
Alperen Yilmaz <theurgeboat@gmail.com>
2024-05-31 02:40:38 +0300

    Armstrong | fix: try-catch block for non-integer inputs

--- src/main/java/Armstrong.java
+++ src/main/java/Armstrong.java
@@ -1,32 +1,47 @@
 import java.util.Scanner;
 import java.lang.Math;
-
 public class Armstrong
 {
     public static void main(String[] args)
     {
         Scanner tara = new Scanner(System.in);
-        int length, sayi, i, armst=0, sayiComp, sum=0;
+        String inp;
+        int length, armst=0, sum=0, temp, inpInt=0;
         int[] intArray = new int[50];
         System.out.print("This program finds if the input number is an Armstrong number, and calculates the sum of digits." +
-                "\nArmstrong number example: if 1234 = 1^4+2^4+3^4+4^4, then 1234 is an Armstrong Number"+
-                "\nEnter a number:");
-        sayi = tara.nextInt();
-        sayiComp = sayi;
-        length = (int) (Math.log10(sayi) + 1);
-        for(i=0;i<length;i++)
+                "\nArmstrong number example: if 1234 = 1^4+2^4+3^4+4^4, then 1234 is an Armstrong Number\n");
+        while (true)
         {
-            intArray[length-i-1] = sayi%10;
-            armst += Math.pow(intArray[length-i-1],length);
-            sum += intArray[length-i-1];
-            sayi/=10;
+            System.out.print("(Press blank enter to exit) Enter a number:");
+            inp = tara.nextLine();
+            if(inp.isEmpty())
+            {
+                System.out.println("Exiting...");
+                break;
+            }
+            try
+            {
+                inpInt=Integer.parseInt(inp.trim());
+                temp=inpInt;
+                length = (int) (Math.log10(inpInt) + 1);
+                for(int i=0;i<length;i++)
+                {
+                    intArray[length-i-1] = temp%10;
+                    armst += Math.pow(intArray[length-i-1],length);
+                    sum += intArray[length-i-1];
+                    temp/=10;
+                }
+                break;
+            }catch (NumberFormatException e) {
+                System.out.println("Invalid input! Please enter a valid number.");
+            }
         }
 
-        if(armst==sayiComp)
-            System.out.printf("The number %d you entered is an Armstrong number.",sayiComp);
+        if(armst==inpInt)
+            System.out.printf("The number %d you entered is an Armstrong number.", inpInt);
         else
-            System.out.printf("The number %d you entered is not an Armstrong number.",sayiComp);
+            System.out.printf("The number %d you entered is not an Armstrong number.", inpInt);
 
-        System.out.printf("\nDigit sum of the number %d is: %d",sayiComp,sum);
+        System.out.printf("\nDigit sum of the number %d is: %d", inpInt, sum);
     }
 }
